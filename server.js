7391d89b8ec6ca54fa4957eb0c941e58f90fa989
mehakpatil0512-crypto <mehakpatil0512@gmail.com>
2026-07-31 12:10:46 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+
+app.use("/css", express.static(path.join(__dirname, "views", "css")));
+app.use("/js", express.static(path.join(__dirname, "views", "css", "js")));
+
+
+app.get("/", (req, res) => {
+
+    res.sendFile(path.join(__dirname, "views", "home.html"));
+});
+
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "login.html"));
+});
+
+
+app.get("/signup", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "signup.html"));
+});
+
+
+app.get("/api/student", (req, res) => {
+    res.json({
+        id: 1,
+        name: "Yash",
+        course: "Python"
+    });
+});
+
+
+
+app.listen(5000, () => {
+    console.log(`Server Running on Port 5000`);
+});
